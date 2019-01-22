e7e525500874c533ef555434be10b5651f7fcca5
Mark Aleksander Hil <mark@100shapes.com>
2019-01-22 12:06:22 +0000

    Working GraphQL endpoint

--- app/app.py
+++ app/app.py
@@ -13,8 +13,7 @@ app.debug = True
 # Configs
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
-os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +  os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
