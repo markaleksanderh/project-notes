354f0f8c81d49c7e27e81a5c19c71b9e68c10f1a
Mark Aleksander Hil <mark@100shapes.com>
2019-01-22 15:19:39 +0000

    Post mutation

--- app/app.py
+++ app/app.py
@@ -58,7 +58,30 @@ class Query(graphene.ObjectType):
     all_posts = SQLAlchemyConnectionField(PostObject)
     all_users = SQLAlchemyConnectionField(UserObject)
 
-schema = graphene.Schema(query=Query)
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        body = graphene.String(required=True)
+        username = graphene.String(required=True)
+
+    post = graphene.Field(lambda: PostObject)
+
+    def mutate(self, info, title, body, username):
+        user = User.query.filter_by(username=username).first()
+        post = Post(title=title, body=body)
+
+        if user is not None:
+            post.author = user
+
+        db.session.add(post)
+        db.session.commit()
+
+        return CreatePost(post=post)
+
+class Mutation(graphene.ObjectType):
+    create_post = CreatePost.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 
 # Routes
