ee83fe8d2ba2024dbd7859a6b809a8185f7d3f29
Mark Aleksander Hil <mark@100shapes.com>
2019-01-22 10:53:41 +0000

    Added models and config

--- app/app.py
+++ app/app.py
@@ -1,17 +1,48 @@
 # https://medium.com/@marvinkome/creating-a-graphql-server-with-flask-ae767c7e2525
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.debug = True
 
-
 # Configs
-# Modules
-# Models
-# Schema Objects
-# Routes
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+# Modules
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+# Schema Objects
+
+# Routes
 @app.route('/')
 def index():
     return '<p>Hello GraphQL</p>'
